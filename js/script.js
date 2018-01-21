3316027402c2ac0aaa71c741821d5421dad336f8
Helihub <haminh.thieu@gmail.com>
2018-01-21 19:20:12 +0100

    rescale

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar').toggleClass('active');
 
-        if($('button span').hasClass('glyphicon-chevron-right')){
-          $(this).html('<span class="glyphicon glyphicon-chevron-left"></span>');
-        } else {
-          $(this).html('<span class="glyphicon glyphicon-chevron-right"></span>'); 
-        }
-    });
+    if ($('button span').hasClass('glyphicon-chevron-right')) {
+      $(this).html('<span class="glyphicon glyphicon-chevron-left"></span>');
+
+    } else {
+      $(this).html('<span class="glyphicon glyphicon-chevron-right"></span>');
+    }
+  });
 
 });
